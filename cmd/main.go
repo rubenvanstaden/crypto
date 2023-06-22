d88b75116d64c1ad7b0eeca4effef92c75ead306
Ruben van Staden <rvs.cipher@proton.me>
2023-06-21 22:30:20 -0400

    move code to cmd dir

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"flag"
+)
+
+func main() {
+
+	flag.Parse()
+	log.SetFlags(0)
+
+	args := flag.Args()
+
+	nsec := ""
+
+	if len(args) > 0 {
+        // Generate a new private, public key pair.
+		if args[0] == "gen" && len(args) == 1 {
+			nsec = args[0]
+		}
+	}
+
+	if nsec != "" {
+        sk := GeneratePrivateKey()
+
+        pk, err := GetPublicKey(sk)
+        if err != nil {
+            log.Fatal("unable to generate public key")
+        }
+
+        log.Printf("nsec: %s", sk)
+        log.Printf("npub: %s", pk)
+    }
+}
